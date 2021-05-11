222dd6fb153b484f1e5093c75222d1a896f2eb30
Daniel Boelman <danielboelman@gmail.com>
2021-05-11 19:35:29 +0200

    Added some changes

--- BaseFunctions/test.c
+++ BaseFunctions/test.c
@@ -6,6 +6,9 @@
 #define MIN_GRAPH_SIZE 10
 #define MAX_GRAPH_SIZE 32
 
+// TODO: Freeze Data
+// TODO: Freeze version
+
 int GRAPH_SIZE = 0;
 /* multiply a matrix and vector and return a vector */
 struct vector *mul_matrix_vector(struct matrix *matrix, struct vector *vector){
@@ -28,23 +31,6 @@ struct vector *mul_matrix_vector(struct matrix *matrix, struct vector *vector){
 
 }
 
-struct vector *mul_matrix_vector_optimized(struct matrix *matrix, struct vector *vector){
-    if (matrix->cols != vector->size) {
-        return NULL;
-    }
-    int rowz = matrix->rows;
-    if (!rowz) {
-        return NULL;
-    }
-    struct vector *result = v_fill(rowz, 0);
-    for (int j = 0; j < rowz; j++) {
-        for (int i = 0; i < vector->size; i++) {
-            *(result->data + j) += (*(vector->data + i) * *(matrix->data + j * rowz + i));
-        }
-    }
-    return result;
-}
-
 /* Basic CC algorithm based on bfs */
 int cc(struct matrix *matrix) {
     struct vector *vector = v_fill(matrix->cols, 0);
@@ -61,6 +47,23 @@ int cc(struct matrix *matrix) {
     }
     v_free(vector);
     return 1;
+}
+
+struct vector *mul_matrix_vector_optimized(struct matrix *matrix, struct vector *vector){
+    if (matrix->cols != vector->size) {
+        return NULL;
+    }
+    int rowz = matrix->rows;
+    if (!rowz) {
+        return NULL;
+    }
+    struct vector *result = v_fill(rowz, 0);
+    for (int j = 0; j < rowz; j++) {
+        for (int i = 0; i < vector->size; i++) {
+            *(result->data + j) += (*(vector->data + i) * *(matrix->data + j * rowz + i));
+        }
+    }
+    return result;
 }
 
 /* Basic CC algorithm based on bfs */
@@ -140,14 +143,12 @@ int cc4(char *matrix) {
                 *(result + j) |= (*(vector + i) & *(matrix + j * GRAPH_SIZE + i));
             }
             d &= (*(vector + j) | *(result + j));
-
         }
         if (d) {
             free(vector);
             free(result);
             return 0;
         }
-
         for (int i = 0; i < GRAPH_SIZE; i++) {
             *(vector + i) = (*(vector + i) | *(result + i));
             *(result + i) = 0;
@@ -161,7 +162,7 @@ int cc4(char *matrix) {
     return 1;
 }
 
-/* Fifth iteration of my CCa
+/* 6 iteration of my CCa
  * This one is the most abstract: uses only 2 allocated data structures based on the graph size
  */
 int cc5(char *matrix) {
@@ -175,7 +176,6 @@ int cc5(char *matrix) {
     do {
         for (int j = 0; j < GRAPH_SIZE; j++) {
             for (int i = 0; i < GRAPH_SIZE; i++) {
-                *(vector + j) |= (*(vector + i) & *(matrix + j * GRAPH_SIZE + i));
                 *(vector + j) |= (*(vector + i) & *(matrix + j * GRAPH_SIZE + i));
             }
             d &= *(vector + j);
@@ -192,7 +192,7 @@ int cc5(char *matrix) {
     return 1;
 }
 
-int cc7(char *matrix) {
+int cc6(char *matrix) {
     char *vector = calloc(GRAPH_SIZE, sizeof(char));
     if(!vector) {
         return 1;
@@ -203,18 +203,6 @@ int cc7(char *matrix) {
     char loop_size = GRAPH_SIZE - 10;
     do {
         for (int j = 0; j < GRAPH_SIZE; j++) {
-            /* First ten entries because min graph size is 10 */
-            // *(vector + j) |= (*(vector ) & *(matrix + j * GRAPH_SIZE));
-            // *(vector + j) |= (*(vector + 1) & *(matrix + j * GRAPH_SIZE + 1));
-            // *(vector + j) |= (*(vector + 2) & *(matrix + j * GRAPH_SIZE + 2));
-            // *(vector + j) |= (*(vector + 3) & *(matrix + j * GRAPH_SIZE + 3));
-            // *(vector + j) |= (*(vector + 4) & *(matrix + j * GRAPH_SIZE + 4));
-            // *(vector + j) |= (*(vector + 5) & *(matrix + j * GRAPH_SIZE + 5));
-            // *(vector + j) |= (*(vector + 6) & *(matrix + j * GRAPH_SIZE + 6));
-            // *(vector + j) |= (*(vector + 7) & *(matrix + j * GRAPH_SIZE + 7));
-            // *(vector + j) |= (*(vector + 8) & *(matrix + j * GRAPH_SIZE + 8));
-            // *(vector + j) |= (*(vector + 9) & *(matrix + j * GRAPH_SIZE + 9));
-
             int i = 0;
             for (; i < loop_size - 1; i+=2) {
                 *(vector + j) |= (*(vector + i) & *(matrix + j * GRAPH_SIZE + i));
@@ -237,83 +225,8 @@ int cc7(char *matrix) {
     return 1;
 }
 
-/* Sixth iteration of my CCa. Try to combine earlier parts of cc3 and cc5
- * This one is the most abstract: uses only 2 allocated data structures based on the graph size
- */
-int cc6(char *matrix) {
-    char *vector = calloc(GRAPH_SIZE, sizeof(char));
-    if(!vector) {
-        return 1;
-    }
-    *(vector) = 1;
-    char d = 1;
-    char iters = 0;
-    do {
-        for (int j = 0; j < GRAPH_SIZE; j++) {
-            for (int i = 0; i < GRAPH_SIZE; i++) {
-                *(vector + j) |= (*(vector + i) * *(matrix + j * GRAPH_SIZE + i));
-            }
-            d += *(vector + j);
-        }
-        if (d == GRAPH_SIZE) {
-            free(vector);
-            return 0;
-        }
-        d = 1;
-        iters++;
-    }
-    while (iters < GRAPH_SIZE);
-    free(vector);
-    return 1;
-}
-
-int cc8(char *matrix) {
-    char *vector = calloc(GRAPH_SIZE, sizeof(char));
-    if(!vector) {
-        return 1;
-    }
-    *(vector) = 1;
-    char d = 1;
-    char iters = 0;
-    char loop_size = GRAPH_SIZE - 10;
-    do {
-        for (int j = 0; j < GRAPH_SIZE; j++) {
-            /* First ten entries because min graph size is 10 */
-            *(vector + j) |= (*(vector ) & *(matrix + j * GRAPH_SIZE));
-            *(vector + j) |= (*(vector + 1) & *(matrix + j * GRAPH_SIZE + 1));
-            *(vector + j) |= (*(vector + 2) & *(matrix + j * GRAPH_SIZE + 2));
-            *(vector + j) |= (*(vector + 3) & *(matrix + j * GRAPH_SIZE + 3));
-            *(vector + j) |= (*(vector + 4) & *(matrix + j * GRAPH_SIZE + 4));
-            *(vector + j) |= (*(vector + 5) & *(matrix + j * GRAPH_SIZE + 5));
-            *(vector + j) |= (*(vector + 6) & *(matrix + j * GRAPH_SIZE + 6));
-            *(vector + j) |= (*(vector + 7) & *(matrix + j * GRAPH_SIZE + 7));
-            *(vector + j) |= (*(vector + 8) & *(matrix + j * GRAPH_SIZE + 8));
-            *(vector + j) |= (*(vector + 9) & *(matrix + j * GRAPH_SIZE + 9));
-
-            int i = 10;
-            for (; i < loop_size - 1; i+=2) {
-                *(vector + j) |= (*(vector + i) & *(matrix + j * GRAPH_SIZE + i));
-                *(vector + j) |= (*(vector + i + 1) & *(matrix + j * GRAPH_SIZE + i + 1));
-            }
-            for ( ; i < loop_size; i++) {
-                *(vector + j) |= (*(vector + i) & *(matrix + j * GRAPH_SIZE + i));
-            }
-            d &= *(vector + j);
-        }
-        if (d) {
-            free(vector);
-            return 0;
-        }
-        d = 1;
-        iters++;
-    }
-    while (iters < GRAPH_SIZE);
-    free(vector);
-    return 1;
-}
-
 /* BEST CASE CURRENTLY */
-int cc9(char *matrix) {
+int cc7(char *matrix) {
     char *vector = calloc(GRAPH_SIZE, sizeof(char));
     if(!vector) {
         return 1;
@@ -343,47 +256,6 @@ int cc9(char *matrix) {
                 *(vector + j) |= (*(vector + i + 2) & *(matrix + j * GRAPH_SIZE + i + 2));
             }
             for (; i < loop_size; i++) {
-                *(vector + j) |= (*(vector + i) & *(matrix + j * GRAPH_SIZE + i));
-            }
-            d &= *(vector + j);
-        }
-        if (d) {
-            free(vector);
-            return 0;
-        }
-        d = 1;
-        iters++;
-    }
-    while (iters < GRAPH_SIZE);
-    free(vector);
-    return 1;
-}
-
-
-int cc10(char *matrix) {
-    char *vector = calloc(GRAPH_SIZE, sizeof(char));
-    if(!vector) {
-        return 1;
-    }
-    *(vector) = 1;
-    char d = 1;
-    char iters = 0;
-    char loop_size = GRAPH_SIZE - 10;
-    do {
-        for (int j = 0; j < GRAPH_SIZE; j++) {
-            /* First ten entries because min graph size is 10 */
-            *(vector + j) |= (*(vector ) & *(matrix + j * GRAPH_SIZE));
-            *(vector + j) |= (*(vector + 1) & *(matrix + j * GRAPH_SIZE + 1));
-            *(vector + j) |= (*(vector + 2) & *(matrix + j * GRAPH_SIZE + 2));
-            *(vector + j) |= (*(vector + 3) & *(matrix + j * GRAPH_SIZE + 3));
-            *(vector + j) |= (*(vector + 4) & *(matrix + j * GRAPH_SIZE + 4));
-            *(vector + j) |= (*(vector + 5) & *(matrix + j * GRAPH_SIZE + 5));
-            *(vector + j) |= (*(vector + 6) & *(matrix + j * GRAPH_SIZE + 6));
-            *(vector + j) |= (*(vector + 7) & *(matrix + j * GRAPH_SIZE + 7));
-            *(vector + j) |= (*(vector + 8) & *(matrix + j * GRAPH_SIZE + 8));
-            *(vector + j) |= (*(vector + 9) & *(matrix + j * GRAPH_SIZE + 9));
-
-            for (int i = 10; i < loop_size; i++) {
                 *(vector + j) |= (*(vector + i) & *(matrix + j * GRAPH_SIZE + i));
             }
             d &= *(vector + j);
@@ -432,21 +304,18 @@ char *readGraph2(char* filename) {
     return matrix;
 }
 
-// TODO: Swapable implementations
-// TODO: Flags
-// TODO: Produce more than one result at a time
 int main (int argc, char *argv[]) {
     (void) argc;
     (void) argv;
     int iterations = 10000;
     int cca = 0;
-    int cca_num = 8;
+    int cca_num = 1;
 
     char outname[256];
 
 
     sprintf(outname, "./results/results_cc%d.csv", cca_num);
-    FILE* fout = fopen(outname, "a+");
+    FILE* fout = fopen(outname, "w");
     fprintf(fout, "cca,size,density,total_time\n");
 
     for (int size = MIN_GRAPH_SIZE; size < MAX_GRAPH_SIZE; size++) {
@@ -458,24 +327,22 @@ int main (int argc, char *argv[]) {
                 char filename[256];
                 sprintf(filename, "./input/%dsize_%ddens_%d.txt", size, density, inputfile);
 
-
-
-                // struct matrix *am = readGraph(filename);
-                char *am = readGraph2(filename);
+                struct matrix *am = readGraph(filename);
+                // char *am = readGraph2(filename);
                 cca = cc8(am);
                 for (int i = 0; i < iterations; i++) {
                     struct timespec begin, end;
                     double cpu_time_used;
                     clock_gettime(CLOCK_REALTIME, &begin);
-                    cca = cc8(am);
+                    cca = cc1(am);
                     clock_gettime(CLOCK_REALTIME, &end);
                     long seconds = end.tv_sec - begin.tv_sec;
                     long nseconds = end.tv_nsec - begin.tv_nsec;
                     cpu_time_used = seconds + nseconds*1e-9;
                     total_time = total_time + cpu_time_used;
                 }
-                // m_free(am);
-                free(am);
+                m_free(am);
+                // free(am);
                 // fprintf(fout, "Input file: %s\n", filename);
                 // fprintf(fout, "Graph size: %d\nEdge density: %d\n", size, density);
                 fprintf(fout, "%d,%d,%d,%.10f\n", cca_num, size, density, total_time);
--- datareader.py
+++ datareader.py
@@ -1,13 +1,22 @@
 import plotly.express as px
 import pandas as pd
-import glob
+import os
+from glob import glob
+
+PATH = "./results_archive/"
+EXT = "*.csv"
+all_csv_files = [file
+                 for path, subdir, files in os.walk(PATH)
+                 for file in glob(os.path.join(path, EXT))]
 
 # TODO: Add some variable data,
 # TODO: Graph swapping maybe?
+# dfs = []
+# for file in glob.glob("./results/*.csv"):
+#     dfs.append(pd.read_csv(file))
 dfs = []
-for file in glob.glob("./results/*.csv"):
+for file in all_csv_files:
     dfs.append(pd.read_csv(file))
-
 result = pd.concat(dfs)
 
 
@@ -16,23 +25,79 @@ result = pd.concat(dfs)
 # df = pd.read_csv('result2s.csv')
 result["cca"] = result["cca"].astype(str)
 result["density"] = result["density"].astype(str)
+
+grouped = result.groupby(result.density)
+
+res_low_dens = grouped.get_group("10")
+res_high_dens = grouped.get_group("20")
+
 print(result.head())
-fig = px.scatter(result, x='size', y='total_time', color="cca",
-                 hover_data=['density'], title="Plot of all results",
-                 symbol="density",
-                 labels={"total_time": "Time for 10000 Iterations",
-                         "size": "Graph Size",
-                         "cca": "Connected Components Version",
-                         "density": "Density"})
-fig2 = px.scatter(result, x='total_time', y='size', color="cca",
-                  hover_data=['density'], title="Plot of all results",
+fig1 = px.scatter(result, x='size', y='total_time', color="cca",
+                  hover_data=['density'], title="Plot of all 7 Basic Versions",
                   symbol="density",
+                  trendline='lowess',
                   labels={"total_time": "Time for 10000 Iterations",
                           "size": "Graph Size",
                           "cca": "Connected Components Version",
-                          "density": "Density"})
-# fig = px.scatter_matrix(result,
-#                         dimensions=["total_time", "size", "density"],
-#                         color="cca")
-fig.show()
+                          "density": "Density"},
+                  category_orders={"cca": ["1", "2", "3", "4", "5", "6", "7"],
+                                   "density": ["10", "20"]})
+
+fig2 = px.scatter(res_low_dens, x='size', y='total_time', color="cca",
+                  hover_data=['density'],
+                  title="Plot of all 7 current implementations, Density=10",
+                  trendline='lowess',
+                  labels={"total_time": "Time for 10000 Iterations",
+                          "size": "Graph Size",
+                          "cca": "Connected Components Version",
+                          "density": "Density"},
+                  category_orders={"cca": ["1", "2", "3", "4", "5", "6", "7"]
+
+                                   })
+
+fig3 = px.scatter(res_high_dens, x='size', y='total_time', color="cca",
+                  hover_data=['density'],
+                  title="Plot of all 7 current implementations, Density=20",
+                  trendline='lowess',
+                  labels={"total_time": "Time for 10000 Iterations",
+                          "size": "Graph Size",
+                          "cca": "Connected Components Version",
+                          "density": "Density"},
+                  category_orders={"cca": ["1", "2", "3",
+                                           "4", "5", "6", "7"]})
+
+
+# fig4 = px.scatter(result, x='size', y='total_time', color="density",
+#                   animation_frame="cca",
+#                   hover_data=['density'],
+#                   title="Interactive plot of CCA performence over iterations",
+#                   symbol="density",
+#                   trendline='lowess',
+#                   labels={"total_time": "Time for 10000 Iterations",
+#                           "size": "Graph Size",
+#                           "cca": "Connected Components Version",
+#                           "density": "Density"},
+#                   category_orders={"cca": ["1", "2", "3", "4", "5", "6", "7"],
+#                                    "density": ["10", "20"]})
+
+# fig5 = px.scatter(result, x='size', y='total_time', color="cca",
+#                   animation_frame="density",
+#                   hover_data=['density'],
+#                   title="Interactive plot of CCA performence over density",
+#                   symbol="density",
+#                   trendline='lowess',
+#                   labels={"total_time": "Time for 10000 Iterations",
+#                           "size": "Graph Size",
+#                           "cca": "Connected Components Version",
+#                           "density": "Density"},
+#                   category_orders={"cca": ["1", "2", "3", "4", "5", "6", "7"],
+#                                    "density": ["10", "20"]})
+
+
+fig1.show()
 fig2.show()
+fig3.show()
+
+# Interactive versions
+# fig4.show()
+# fig5.show()
